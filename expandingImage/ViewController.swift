3ae965f06edaf717beb9ebe9edbe4fd57e24be28
Rajdeep Kaur <chahal.rajdeep03@gmail.com>
2020-01-13 19:50:24 -0500

    expanding image

--- expandingImage/ViewController.swift
+++ expandingImage/ViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var myView: UIView!
+    
+    @IBOutlet weak var lblCounter: UILabel!
+    var timer: Timer!
+    var counter = 0
+   
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let mainViewHeight = view.frame.size.height
+    let mainViewWidth = view.frame.size.width
+    var myViewHeight = self.myView.frame.size.height
+    var myViewWidth = self.myView.frame.size.width
+    myView.center = view.center
+    myView.backgroundColor = .red
+    lblCounter.text = "\(counter)"
+    var timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true)
+    { timer in
+          self.myView.backgroundColor = .random()
     }
-
-
+    expand()
+    }
+ 
+   
+  func expand(){
+    var timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true)
+    { timer in
+         // self.myView.backgroundColor = .random()
+               print("Expand by\(timer)")
+    self.myView.frame.origin.x -= 5
+    self.myView.frame.origin.y -= 5
+    self.myView.frame.size.height += 10
+    self.myView.frame.size.width += 10
+    if(self.myView.frame.size.width == 390)
+    {
+      timer.invalidate()
+      self.shrink()
+    }
+    }
+    
+  }
+  func shrink(){
+//    self.counter += 1
+//    self.lblCounter.text = "\(self.counter)"
+   var timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true)
+   { timer in
+     //self.myView.backgroundColor = .random()
+           print("Expand by\(timer)")
+    self.myView.frame.origin.x += 5
+    self.myView.frame.origin.y += 5
+    self.myView.frame.size.height -= 10
+    self.myView.frame.size.width -= 10
+    if(self.myView.frame.size.width == 50)
+    {
+       timer.invalidate()
+       
+      self.expand()
+      self.counter += 1
+      self.lblCounter.text = "\(self.counter)"
+    }
+  }
+  }
 }
-
+extension UIColor {
+  static func random() -> UIColor {
+    func random() -> CGFloat { return .random(in:0...1) }
+    return UIColor(red:  random(),
+            green: random(),
+            blue: random(),
+            alpha: 1.0)
+  }
+}
